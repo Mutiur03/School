72314cd013cfa40f293fa6acfc5f603567b9dc56
Mutiur03 <mutiur5bb@gmail.com>
2025-05-05 03:05:37 +0600

    don's believe in last one

--- admin/src/pages/Attendence.jsx
+++ admin/src/pages/Attendence.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button } from "../components/ui/Button";
+import { Button } from "../components/ui/button";
 
 function Attendance() {
   const [students, setStudents] = useState([]);
