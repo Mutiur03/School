3256df5aa0df25f2aef760eb64373c8fba57694f
Mutiur03 <mutiur5bb@gmail.com>
2025-07-27 01:27:52 +0600

    Login issue

--- server/server.js
+++ server/server.js
@@ -30,6 +30,7 @@ const allowedOrigins = [
   "https://school-students.onrender.com",
   "https://school-admin-ae6r.onrender.com",
   "https://teacherdashboardlbp.vercel.app",
+  "https://teacher.lbp.mutiurrahman.com",
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
