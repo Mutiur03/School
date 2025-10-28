17c30e52cf8fd67be12f37d4197f8520d72ed33e
Mutiur <mutiur5bb@gmail.com>
2025-10-28 23:15:39 +0600

    docker

---
+++ server/restart.sh
@@ -0,0 +1,12 @@
+#docker network create school_network
+
+docker compose  -f backend-run.yml down
+docker system df
+docker compose  -f backend-run.yml up -d --build
+docker exec school_backend npm run build
+docker system df
+docker compose  -f backend-run.yml logs -f
+#docker system prune -a -f
+docker builder prune -f
+#docker image prune -f
+docker system df
