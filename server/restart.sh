069711e735da6e83ab4c6d6b65196d6e40eae07c
Mutiur <mutiur5bb@gmail.com>
2025-12-07 04:36:19 +0600

    docker

--- server/restart.sh
+++ server/restart.sh
@@ -6,6 +6,6 @@ docker compose  -f backend-run.yml up -d --build
 docker exec school_backend npm run build
 docker system df
 docker compose  -f backend-run.yml logs -f
-docker system prune -a -f
-docker builder prune -f
+# docker system prune -a -f
+# docker builder prune -f
 docker system df
