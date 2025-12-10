c95418483e57c724b93d69d20e330c34f769d2c3
Mutiur <mutiur5bb@gmail.com>
2025-12-10 19:26:49 +0600

    size matters

--- server/routes/admissionResultRoutes.js
+++ server/routes/admissionResultRoutes.js
@@ -31,9 +31,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: {
-    fileSize: 10 * 1024 * 1024, 
-  },
+  // limits: {
+  //   fileSize: 10 * 1024 * 1024, 
+  // },
   fileFilter: (req, file, cb) => {
     if (file.mimetype === "application/pdf") {
       cb(null, true);
