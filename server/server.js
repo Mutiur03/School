47940a2ec7bae19e4dd4fcb3a9566ccc2b1df1ea
Mutiur <mutiur5bb@gmail.com>
2025-12-13 19:29:14 +0600

    Analytics

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const storagePath = path.join(__dirname, "uploads");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const envAllowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(",")
+  ? process.env.ALLOWED_ORIGINS.split(",").trim()
   : null;
 const allowedOrigins = envAllowedOrigins;
 app.use(
