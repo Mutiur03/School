e622c4674d930799a5091f47aff053d1dfeb56e3
Mutiur <mutiur5bb@gmail.com>
2026-01-26 13:47:43 +0600

    Done

--- client/src/pages/HeadMsg.tsx
+++ client/src/pages/HeadMsg.tsx
@@ -1,48 +1,36 @@
-import axios from 'axios';
-import React, { useEffect } from 'react'
-interface Head {
-    head_message: string;
-    teacher: {
-        name: string;
-        image: string;
-    }
-}
+import { useHeadMasterMsg } from "@/hooks/useSchoolData";
+import backend from "@/lib/backend";
+import { useEffect } from "react";
+
 function HeadMsg() {
-    useEffect(() => {
-        document.title = "Message From Headmaster";
-    }, []);
-    const host = import.meta.env.VITE_BACKEND_URL;
-    const [head, setHead] = React.useState<Head | null>(null);
-    const [imgLoading, setImgLoading] = React.useState<boolean>(true);
-    useEffect(() => {
-        axios.get('/api/teachers/get_head_msg').then(response => {
-            console.log(response.data);
-            setHead(response.data || null);
-            setImgLoading(false);
-        }).catch(error => {
-            console.error('Error fetching head message:', error);
-        });
-    }, [])
-    return (
-        <div className="flex flex-col items-center h-screen pt-12">
-            <h2 className='mb-5 text-4xl underline'>Message From Headmaster</h2>
-            <div className="p-4 mb-5 border border-gray-100  shadow-lg">
-                {imgLoading ? (
-                    <div className="w-42 h-42 mb-5 bg-gray-300 animate-pulse shadow-2xl"></div>
-                ) : (
-                    <img
-                        src={head?.teacher.image ? `${host}/${head.teacher.image}` : '/placeholder.svg'}
-                        alt="Head Image"
-                        className="w-42 h-42 object-cover object-top "
-                    />
-                )}
-            </div>
-            <h1 className="text-2xl font-bold mb-4">{head?.teacher.name ?? ''}</h1>
-            <div className=" text-justify max-w-2xl w-full">
-                <p>{head?.head_message ?? ''}</p>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    document.title = "Message From Headmaster";
+  }, []);
+  const { data: head, isLoading } = useHeadMasterMsg();
+  return (
+    <div className="flex flex-col items-center h-screen pt-12">
+      <h2 className="mb-5 text-4xl underline">Message From Headmaster</h2>
+      <div className="p-4 mb-5 border border-gray-100  shadow-lg">
+        {isLoading ? (
+          <div className="w-42 h-42 mb-5 bg-gray-300 animate-pulse shadow-2xl"></div>
+        ) : (
+          <img
+            src={
+              head?.teacher.image
+                ? `${backend}/${head.teacher.image}`
+                : "/placeholder.svg"
+            }
+            alt="Head Image"
+            className="w-42 h-42 object-cover object-top "
+          />
+        )}
+      </div>
+      <h1 className="text-2xl font-bold mb-4">{head?.teacher.name ?? ""}</h1>
+      <div className=" text-justify max-w-2xl w-full">
+        <p>{head?.head_message ?? ""}</p>
+      </div>
+    </div>
+  );
 }
 
-export default HeadMsg
+export default HeadMsg;
