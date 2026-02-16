abbcf750a40329a3f55527adce8a21d4a949fe82
Mutiur <mutiur5bb@gmail.com>
2026-02-16 17:27:56 +0600

    tanstaack on class 6 reg

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -4,15 +4,28 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { UnifiedAuthProvider } from "./context/unifiedAuthContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true, // Re-fetches data when you switch browser tabs and come back
+      staleTime: 300000, // Data is considered stale after 5 minutes, allowing prompt background refetching
+      refetchOnReconnect: true,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <UnifiedAuthProvider>
-      <BrowserRouter>
-        {/* <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme"> */}
-        <App />
-        {/* </ThemeProvider> */}
-      </BrowserRouter>
-    </UnifiedAuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <UnifiedAuthProvider>
+        <BrowserRouter>
+          {/* <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme"> */}
+          <App />
+          {/* </ThemeProvider> */}
+        </BrowserRouter>
+      </UnifiedAuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
