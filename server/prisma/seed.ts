10f04c2ca94740eb1d8a2924bfd8e3fc95a572aa
Mutiur <mutiur5bb@gmail.com>
2026-03-07 10:45:37 +0600

    build issue

---
+++ server/prisma/seed.ts
@@ -0,0 +1,27 @@
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
+
+const prisma = new PrismaClient();
+async function main() {
+  await prisma.categories.createMany({
+    data: [
+      { category: "Event" },
+      { category: "Campus" },
+      { category: "Labs" },
+      { category: "Achievement" },
+    ],
+    skipDuplicates: true,
+  });
+}
+
+main()
+  .then(() => {
+    console.log("✅ Seeding completed.");
+  })
+  .catch((e) => {
+    console.error("❌ Seeding error:", e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
