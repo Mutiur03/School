53047c4e3e13acfa6cf4d6a9e77ba34949546775
Mutiur <mutiur5bb@gmail.com>
2026-05-09 17:46:25 +0600

    chore: remove edge runtime export from layout and page components

--- client-next/app/(root)/page.tsx
+++ client-next/app/(root)/page.tsx
@@ -2,8 +2,6 @@ import { Chart } from "@/components/Chart";
 import { ExtraHome } from "@/components/ExtraHome";
 import { NoticeBoard } from "@/components/NoticeBoard";
 
-export const runtime = "edge";
-
 export default function Home() {
   return (
     <>
